fcef730de69fbe6379049cdc5b8ff1350f80dea6
Doan Dinh <doandoinh@usf.edu>
2025-06-22 13:17:32 -0400

    created calculateBasePay function

--- cc_5.js
+++ cc_5.js
@@ -3,3 +3,8 @@ const employees = [
   { name: "Tuan", hourlyRate: 19, hoursWorked: 44 },
   { name: "Emily", hourlyRate: 18, hoursWorked: 50 }
 ];
+
+function calculateBasePay(rate, hours) {
+  const regularHours = Math.min(hours, 40);
+  return rate * regularHours;
+}
