d372a774bb6b7845f3c6a99d9acf68750cdf9904
Doan Dinh <doandoinh@usf.edu>
2025-06-22 13:07:45 -0400

    added employee records with hourly rate and hours worked

---
+++ cc_5.js
@@ -0,0 +1,5 @@
+const employees = [
+  { name: "Jerry", hourlyRate: 21, hoursWorked: 36 },
+  { name: "Tuan", hourlyRate: 19, hoursWorked: 44 },
+  { name: "Emily", hourlyRate: 18, hoursWorked: 50 }
+];
